5d0a8d800d88b70eceed627f0a9b0dcf41160ffc
Melinda <mjkobaya@gmail.com>
2016-04-13 17:26:36 -0700

    Revert "Committing changes but I might roll them back"
    This reverts commit 91cab60d2f93ce4236bfa0d0c6217efa95fdd4b6.

--- pa1/public_html/formValidation.js
+++
@@ -1,8 +0,0 @@
-function validateForm() {
-    var x = document.forms["form"]["fname"].value;
-    if (x == null || x == "") {
-        alert("Name must be filled out");
-        return false;
-    }
-}
-
