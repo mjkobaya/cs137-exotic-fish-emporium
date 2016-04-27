f3ba81505e34e24442cd272a09fc5ca0405435de
Melinda <mjkobaya@gmail.com>
2016-04-27 16:55:12 -0700

    PHP scripts for connection to MySQL

---
+++ pa2/connect.php
@@ -0,0 +1,19 @@
+<?php
+ini_set('display_errors','on');
+
+$hostname = 'sylvester-mccoy-v3.ics.uci.edu';
+$username = 'inf124grp16';
+$password = 'n?yUmap3';
+$database = 'inf124grp16';
+
+
+try{
+        $conn = new PDO("mysql:host=$hostname;dbname=$database;", $username, $password);
+        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        echo "Connected successfully<br />";
+} catch (PDOException $e) {
+        echo "Connection failed: " . $e->getMessage();
+}
+
+$link = null;
+?>
