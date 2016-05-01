217c2fbf9cc6fc3c253b167d4b70edc46a100c10
Melinda <mjkobaya@gmail.com>
2016-05-01 15:39:22 -0700

    zipcode.php

--- public_html/php/zipcode.php
+++ public_html/php/zipcode.php
@@ -9,9 +9,10 @@ $stmt->bindParam(":zipcode", $zipcode);
 $zipcode = $_POST["zip"];
 $stmt->execute();
 
-while ($row = $stmt->fetch(PDO::FETCH_ASSOC)){
-    print_r($row);
-    //echo "{ state : " . $row['state'] . ", city : " . $row['city'] . " }";
-}
+// while ($row = $stmt->fetch(PDO::FETCH_ASSOC)){
+//     print_r($row);
+// }
+
+print_r($stmt->fetch(PDO::FETCH_ASSOC));
 
 ?>
