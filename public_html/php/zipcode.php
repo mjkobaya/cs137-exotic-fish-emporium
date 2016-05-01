a023228a9a6ba92f21030654addbef2e44dfe6ec
Melinda <mjkobaya@gmail.com>
2016-05-01 14:58:31 -0700

    zipcode.php

--- public_html/php/zipcode.php
+++ public_html/php/zipcode.php
@@ -1,14 +1,16 @@
 <?php
 // Use get request to get city, state matching supplied zipcode
 
-/*require_once("../connect.php");
-
+require_once("../connect.php");
+/*
 $stmt = $conn->prepare("SELECT state, city FROM zipcodes WHERE zip = :zipcode");
 $stmt->bindParam(":zipcode", $zipcode);
 
 $zipcode = $_POST["zip"];
 $stmt->execute();
 */
+
+echo $_POST["zip"];
 echo "Does this work?";
 // while ($row = $stmt->fetch(PDO::FETCH_ASSOC)){
 //     echo "{ state : " . $row['state'] . ", city : " . $row['city'] . " }";
