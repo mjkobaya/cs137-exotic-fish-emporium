7245f59fac54e5f79dc3bd8439ebbaa4f421abb1
Melinda <mjkobaya@gmail.com>
2016-05-01 21:19:52 -0700

    working on confirmation and fixing form_database.php

--- public_html/form_database.php
+++ public_html/form_database.php
@@ -19,36 +19,6 @@
       <!-- Javascript for Ajax call -->
       <script src="js/zipcode_ajax.js"></script>
 
-      <?php 
-        ini_set('display_errors', 1);
-        require_once("connect.php");
-
-        $stmt = $conn->prepare("INSERT INTO purchase_info (FullName, Addr, Cityinfo, State, Zip, Phone, Creditcard, Shipping, Quantity) VALUES (:pname,:addr,:pcity,:pstate,:pzip,:pphone,:credit,:ship,:quant)"); 
-        $stmt->bindParam(':pname', $pname);
-        $stmt->bindParam(':addr', $addr);
-        $stmt->bindParam(':pcity', $pcity);
-        $stmt->bindParam(':pstate', $pstate);
-        $stmt->bindParam(':pzip', $pzip);
-        $stmt->bindParam(':pphone', $pphone);
-        $stmt->bindParam(':credit', $credit);
-        $stmt->bindParam(':ship', $ship);
-        $stmt->bindParam(':quant', $quant);
-
-        $pname = test_input($_POST['name']);
-        $addr = test_input($_POST['address']);
-        $pcity = test_input($_POST["city"]);
-        $pstate = test_input($_POST['state']);
-        $pzip = test_input($_POST['zip']);
-        $pphone = test_input($_POST['phone']);
-        $credit = test_input($_POST['creditCard']);
-        $ship = test_input($_POST['shipMethod']);
-        $quant = test_input($_POST['quantity']);
-
-        if ($stmt->execute()) {
-            // show confirmation
-            ?>
-            <html>
-
     </head>
 
     <body style="background-color:rgba(0, 255, 0, 0)">
@@ -121,7 +91,34 @@
         </div>
         </body>
         </html>
-        <?php
+
+    <?php 
+    ini_set('display_errors', 1);
+    require_once("connect.php");
+
+    $stmt = $conn->prepare("INSERT INTO purchase_info (FullName, Addr, Cityinfo, State, Zip, Phone, Creditcard, Shipping, Quantity) VALUES (:pname,:addr,:pcity,:pstate,:pzip,:pphone,:credit,:ship,:quant)"); 
+    $stmt->bindParam(':pname', $pname);
+    $stmt->bindParam(':addr', $addr);
+    $stmt->bindParam(':pcity', $pcity);
+    $stmt->bindParam(':pstate', $pstate);
+    $stmt->bindParam(':pzip', $pzip);
+    $stmt->bindParam(':pphone', $pphone);
+    $stmt->bindParam(':credit', $credit);
+    $stmt->bindParam(':ship', $ship);
+    $stmt->bindParam(':quant', $quant);
+
+    $pname = test_input($_POST['name']);
+    $addr = test_input($_POST['address']);
+    $pcity = test_input($_POST["city"]);
+    $pstate = test_input($_POST['state']);
+    $pzip = test_input($_POST['zip']);
+    $pphone = test_input($_POST['phone']);
+    $credit = test_input($_POST['creditCard']);
+    $ship = test_input($_POST['shipMethod']);
+    $quant = test_input($_POST['quantity']);
+
+    if ($stmt->execute()) {
+        // show confirmation
     }
     else {
         // failure; display error
@@ -138,6 +135,6 @@
     	$data = htmlspecialchars($data);
     	return $data;
     }      
-?>
+    ?>
 
 
