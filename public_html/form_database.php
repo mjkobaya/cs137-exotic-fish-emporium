39ff8e330dab0586455c7e0fa28f4bda699715e3
Melinda <mjkobaya@gmail.com>
2016-05-01 21:00:59 -0700

    working on confirmation and fixing form_database.php

--- public_html/form_database.php
+++ public_html/form_database.php
@@ -1,3 +1,86 @@
+<html>
+   <meta name="viewport" content="width=device-width, initial-scale=1">
+   <link rel="stylesheet" href="http://www.w3schools.com/lib/w3.css">
+   <link rel="stylesheet" href="css/main.css" type="text/css">
+   <link href='https://fonts.googleapis.com/css?family=Playfair+Display:400,700' rel='stylesheet' type='text/css'>
+   
+   <head>
+
+      <title>Fish Forever - Angelfish</title>
+      <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+      <!-- custom CSS -->
+      <link rel="stylesheet" href="css/main.css" type="text/css">
+      
+      <!-- jQuery CDN -->
+      <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
+
+      <!-- Javascript for Ajax call -->
+      <script src="js/zipcode_ajax.js"></script>
+
+    </head>
+
+    <body style="background-color:rgba(0, 255, 0, 0)">
+
+    <style>
+         
+   html
+   {
+    background: url("img/altBackground1.png"); 
+
+    background-size:100% auto;
+    background-repeat:no-repeat;
+    background-attachment: fixed;
+      }
+         table, th, td {
+         border-collapse: collapse;
+         background-color:rgba(0, 0, 0, 0);
+
+         }
+         input{
+          width: 250px;display: block;
+         }
+         th, td {
+         margin: 5px;
+         text-align: left;    
+         }
+         formB
+         {
+         border-radius: 5px;
+         background-color: #f2f2f2;
+         margin: 40px;
+         }
+         input[type=text], select {
+         width: 250px;
+         padding: 12px 20px;
+         margin: 8px 0;
+         display: inline-block;
+         border: 1px solid #ccc;
+         border-radius: 4px;
+         box-sizing: border-box;
+         }
+         .mySlides {display:none}
+         .w3-left, .w3-right, .w3-badge {cursor:pointer}
+         .w3-badge {height:13px;width:13px;padding:0}
+          img
+        {
+           border-radius: 50px;
+        }
+      </style>
+   
+      <section style="font-family:Arial, Helvetica, sans-serif;" id="top-navbar">
+         <ul style="position:relative;bottom:5px;font-size:110%;padding:default">
+            <li><a href="home.html">Home</a></li>
+            <li><a href="home.html#about">About Us</a></li>
+            <li><a href="store.html">Store</a></li>
+            <li style="margin:0;padding:5px 15px 0px 15px; display:inline; width:5em;float:right"id="logo-img-li" ><a style="width:120%;"href="home.html"><img class="rotate" id="logo-img" src="img/fish-logo.png"/></a></li>
+            <li class="right">
+               <h1 style="font-weight:bold;font-size:150%">Fish Forever</h1>
+            </li>
+         </ul>
+      </section>
+</html>
 
 <?php 
 	ini_set('display_errors', 1);
@@ -46,9 +129,7 @@
     	$data = stripslashes($data);
     	$data = htmlspecialchars($data);
     	return $data;
-    }
-
-        
+    }      
 ?>
 
 
